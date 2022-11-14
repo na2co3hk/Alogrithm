85cbe91825f376c3958d1f2f2acfa857823032d8
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2022-11-14 16:10:00 +0800

    Update 前缀和与差分.cpp

--- 其他/前缀和与差分.cpp
+++ 其他/前缀和与差分.cpp
@@ -1,7 +1,5 @@
 //前缀和：存储前n个数的和的数组
 #include<bits/stdc++.h>
-#include<unordered_map>
-#include<unordered_set>
 
 using namespace std;
 
@@ -9,14 +7,18 @@ int n = 5;
 //前缀和初始化
 vector<int>sum(n + 1, 0);
 
-//一维前缀和
-//sum[n]为前n个元素的和
+int range(int l, int r) //前缀和求区间和[l,r]
+{
+	return sum[r] - sum[l - 1];
+}
+
 int main()
 {
 	vector<int>nums = { 1, 2, 3, 4, 5 };
 	//前缀和输入
-	for (int i = 1; i <= n; i++)sum[i] = sum[i - 1] + nums[i - 1]; //下标从1开始
+	for (int i = 1; i <= n; i++)sum[i] = sum[i - 1] + nums[i - 1];
+	for (auto x : sum)cout << x << ' ';
+	cout << endl;
+	cout << range(1, 5);
 	return 0;
 }
-  
-//一维前缀和求区间和

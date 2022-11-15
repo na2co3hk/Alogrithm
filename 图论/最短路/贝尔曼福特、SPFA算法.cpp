71ff7f22370e71a4e02a8860c083a2271388b330
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2022-11-15 11:02:35 +0800

    Create 贝尔曼福特、SPFA算法.cpp

---
+++ 图论/最短路/贝尔曼福特、SPFA算法.cpp
@@ -0,0 +1,159 @@
+int n, m;       // n表示点数，m表示边数
+int dist[N];        // dist[x]存储1到x的最短路距离
+
+struct Edge     // 边，a表示出点，b表示入点，w表示边的权重
+{
+    int a, b, w;
+}edges[M];
+
+// 求1到n的最短路距离，如果无法从1走到n，则返回-1。
+int bellman_ford()
+{
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+
+    // 如果第n次迭代仍然会松弛三角不等式，就说明存在一条长度是n+1的最短路径，由抽屉原理，路径中至少存在两个相同的点，说明图中存在负权回路。
+    for (int i = 0; i < n; i ++ )
+    {
+        for (int j = 0; j < m; j ++ )
+        {
+            int a = edges[j].a, b = edges[j].b, w = edges[j].w;
+            if (dist[b] > dist[a] + w)
+                dist[b] = dist[a] + w;
+        }
+    }
+
+    if (dist[n] > 0x3f3f3f3f / 2) return -1;//如果经过负权边，答案一样会被更新变成不是无穷，所以要变成一个比较大的数
+    return dist[n];
+}
+
+//限制k条边的贝尔曼福特算法
+const int N = 510, M = 10010;
+
+struct Edge {
+    int a;
+    int b;
+    int w;
+} e[M];//把每个边保存下来即可
+int dist[N];
+int back[N];//备份数组防止串联
+int n, m, k;//k代表最短路径最多包涵k条边
+
+int bellman_ford() {
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    for (int i = 0; i < k; i++) {//k次循环
+        memcpy(back, dist, sizeof dist);
+        for (int j = 0; j < m; j++) {//遍历所有边
+            int a = e[j].a, b = e[j].b, w = e[j].w;
+            dist[b] = min(dist[b], back[a] + w);//用上一次迭代的结果更新当前的距离，防止发生串联
+            //使用backup:避免给a更新后立马更新b, 这样b一次性最短路径就多了两条边出来
+        }
+    }
+    if (dist[n] > 0x3f3f3f3f / 2) return -1;
+    else return dist[n];
+
+}
+
+//主函数
+int main() {
+    scanf("%d%d%d", &n, &m, &k);
+    for (int i = 0; i < m; i++) {
+        int a, b, w;
+        scanf("%d%d%d", &a, &b, &w);
+        e[i] = {a, b, w};
+    }
+    int res = bellman_ford();
+    if (dist[n] > 0x3f3f3f3f / 2) puts("impossible");
+    else cout << res;
+
+    return 0;
+}
+
+int n;      // 总点数
+int h[N], w[N], e[N], ne[N], idx;       // 邻接表存储所有边
+int dist[N];        // 存储每个点到1号点的最短距离
+bool st[N];     // 存储每个点是否在队列中
+
+//SPFA算法
+// 求1号点到n号点的最短路距离，如果从1号点无法走到n号点则返回-1
+int spfa()
+{
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+
+    queue<int> q;
+    q.push(1);
+    st[1] = true;
+
+    while (q.size())
+    {
+        auto t = q.front();
+        q.pop();
+
+        st[t] = false;
+
+        for (int i = h[t]; i != -1; i = ne[i])
+        {
+            int j = e[i];
+            if (dist[j] > dist[t] + w[i])
+            {
+                dist[j] = dist[t] + w[i];
+                if (!st[j])     // 如果队列中已存在j，则不需要将j重复插入
+                {
+                    q.push(j);
+                    st[j] = true;
+                }
+            }
+        }
+    }
+
+    if (dist[n] == 0x3f3f3f3f) return -1;
+    return dist[n];
+}
+
+//SPFA算法判断是否存在负环
+int n;      // 总点数
+int h[N], w[N], e[N], ne[N], idx;       // 邻接表存储所有边
+int dist[N], cnt[N];        // dist[x]存储1号点到x的最短距离，cnt[x]存储1到x的最短路中经过的点数
+bool st[N];     // 存储每个点是否在队列中
+
+// 如果存在负环，则返回true，否则返回false。
+bool spfa()
+{
+    // 不需要初始化dist数组
+    // 原理：如果某条最短路径上有n个点（除了自己），那么加上自己之后一共有n+1个点，由抽屉原理一定有两个点相同，所以存在环。
+
+    queue<int> q;
+    for (int i = 1; i <= n; i ++ )//1一号点不一定能到达负环，得把所有点都放进队列判断
+    {
+        q.push(i);
+        st[i] = true;
+    }
+
+    while (q.size())
+    {
+        auto t = q.front();
+        q.pop();
+
+        st[t] = false;
+
+        for (int i = h[t]; i != -1; i = ne[i])
+        {
+            int j = e[i];
+            if (dist[j] > dist[t] + w[i])
+            {
+                dist[j] = dist[t] + w[i];
+                cnt[j] = cnt[t] + 1;
+                if (cnt[j] >= n) return true;       // 如果从1号点到x的最短路中包含至少n个点（不包括自己），则说明存在环
+                if (!st[j])
+                {
+                    q.push(j);
+                    st[j] = true;
+                }
+            }
+        }
+    }
+
+    return false;
+}

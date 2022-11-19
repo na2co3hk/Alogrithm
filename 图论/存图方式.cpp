7229ae45056798dd85fe2ecd5b7fe83b72e7caa7
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2022-11-19 23:32:09 +0800

    Create 存图方式.cpp

---
+++ 图论/存图方式.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+#include<queue>
+
+using namespace std;
+
+const int N = 10010;
+
+//邻接矩阵
+int g[N][N];
+//邻接表
+int h[N], e[N], ne[N], w[N], idx = 0;
+void add(int x, int y, int c)//x->y的权重c
+{
+	e[idx] = y;
+	w[idx] = c;
+	ne[idx] = h[x];
+	h[x] = idx++;	
+}
+
+bool st[N];//检查是否走过这个点
+
+//图的DFS
+void dfs(int u) //u为起点
+{
+	st[u] = true;
+	for (int i = h[u]; ~i ; i = ne[i])
+	{
+		int j = e[i]; //j为终点
+		int c = w[i]; //c为x->y的权重
+		if (!st[j])dfs(j);
+	}
+}
+
+int main()
+{
+	memset(g, 0x3f, sizeof g);
+	memset(h, -1, sizeof h); //非常重要！！！！！！！
+	//邻接矩阵
+	for (int i = 0; i < 5; i++)
+	{
+		int x = a[i][0];
+		int y = a[i][1];
+		int dist = a[i][2];
+		g[x][y] = dist;
+	}
+
+	//邻接表
+	for (int i = 0; i < 5; i++)add(a[i][0], a[i][1], a[i][2]);
+  
+	dfs(a[0][0]);
+	memset(st, false, sizeof st);
+	//图的BFS
+	queue<int>q;
+	st[1] = true;
+	q.push(1);
+	while (!q.empty())
+	{
+		int t = q.front();
+		q.pop();
+		for (int i = h[t]; ~i; i = ne[i])
+		{
+			int j = e[i];
+			int c = w[i];
+			if (!st[j])
+			{
+				st[j] = true;
+				q.push(j);
+			}
+		}
+	}
+
+	return 0;
+}

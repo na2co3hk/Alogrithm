39ce3d25ed52752bc1aca92e0e944ad1f5db58d7
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2023-03-17 18:52:05 +0800

    Update 哈希集合与哈希映射实现.cpp

--- 哈希表/哈希集合与哈希映射实现.cpp
+++ 哈希表/哈希集合与哈希映射实现.cpp
@@ -1,3 +1,42 @@
+//哈希表快速实现
+//拉链法
+int h[N], e[N], ne[N], idx;
+
+// 向哈希表中插入一个数
+void insert(int x)
+{
+    int k = (x % N + N) % N;
+    e[idx] = x;
+    ne[idx] = h[k];
+    h[k] = idx ++ ;
+}
+
+// 在哈希表中查询某个数是否存在
+bool find(int x)
+{
+    int k = (x % N + N) % N;//+N再modN的目的是让余数变成正数
+    for (int i = h[k]; i != -1; i = ne[i])
+        if (e[i] == x)
+            return true;
+
+    return false;
+}
+
+//开放寻址法
+int h[N];
+
+// 如果x在哈希表中，返回x的下标；如果x不在哈希表中，返回x应该插入的位置
+int find(int x)
+{
+    int t = (x % N + N) % N;
+    while (h[t] != NULL && h[t] != x)
+    {
+        t ++ ;
+        if (t == N) t = 0;
+    }
+    return t;
+}
+
 //哈希集合
 #define MAX_LEN 100000          // the amount of buckets
 class MyHashSet {
@@ -175,3 +214,4 @@ int main() {
         cout << "hash map is empty now!" << endl;
     }
 }
+

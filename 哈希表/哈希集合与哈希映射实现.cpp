059bade9af136798ce72503534f077850853194b
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2023-03-17 18:49:04 +0800

    Create 哈希集合与哈希映射实现.cpp

---
+++ 哈希表/哈希集合与哈希映射实现.cpp
@@ -0,0 +1,177 @@
+//哈希集合
+#define MAX_LEN 100000          // the amount of buckets
+class MyHashSet {
+private:
+    vector<int> set[MAX_LEN];   // hash set implemented by array
+    
+    /** Returns the corresponding bucket index. */
+    int getIndex(int key) {
+        return key % MAX_LEN;
+    }
+    
+    /** Search the key in a specific bucket. Returns -1 if the key does not existed. */
+    int getPos(int key, int index) {
+        // Each bucket contains a list. Iterate all the elements in the bucket to find the target key.
+        for (int i = 0; i < set[index].size(); ++i) {
+            if (set[index][i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+public:
+    /** Initialize your data structure here. */
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos < 0) {
+            // Add new key if key does not exist.
+            set[index].push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos >= 0) {
+            // Remove the key if key exists.
+            set[index].erase(set[index].begin() + pos);
+        }
+    }
+    
+    /** Returns true if this set did not already contain the specified element */
+    bool contains(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        return pos >= 0;
+    }
+};
+//哈希集合的用法
+#include <unordered_set>                // 0. include the library
+
+int main() {
+    // 1. initialize a hash set
+    unordered_set<int> hashset;   
+    // 2. insert a new key
+    hashset.insert(3);
+    hashset.insert(2);
+    hashset.insert(1);
+    // 3. delete a key
+    hashset.erase(2);
+    // 4. check if the key is in the hash set
+    if (hashset.count(2) <= 0) {
+        cout << "Key 2 is not in the hash set." << endl;
+    }
+    // 5. get the size of the hash set
+    cout << "The size of hash set is: " << hashset.size() << endl; 
+    // 6. iterate the hash set
+    for (auto it = hashset.begin(); it != hashset.end(); ++it) {
+        cout << (*it) << " ";
+    }
+    cout << "are in the hash set." << endl;
+    // 7. clear the hash set
+    hashset.clear();
+    // 8. check if the hash set is empty
+    if (hashset.empty()) {
+        cout << "hash set is empty now!" << endl;
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+//哈希映射
+#define MAX_LEN 100000            // the amount of buckets
+
+class MyHashMap {
+private:
+    vector<pair<int, int>> map[MAX_LEN];       // hash map implemented by array
+    
+    /** Returns the corresponding bucket index. */
+    int getIndex(int key) {
+        return key % MAX_LEN;
+    }
+    
+    /** Search the key in a specific bucket. Returns -1 if the key does not existed. */
+    int getPos(int key, int index) {
+        // Each bucket contains a vector. Iterate all the elements in the bucket to find the target key.
+        for (int i = 0; i < map[index].size(); ++i) {
+            if (map[index][i].first == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        
+    }
+    
+    /** value will always be positive. */
+    void put(int key, int value) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos < 0) {
+            map[index].push_back(make_pair(key, value));
+        } else {
+            map[index][pos].second = value;
+        }
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos < 0) {
+            return -1;
+        } else {
+            return map[index][pos].second;
+        }
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos >= 0) {
+            map[index].erase(map[index].begin() + pos);
+        }
+    }
+};
+//哈希映射的用法
+#include <unordered_map>                // 0. include the library
+
+int main() {
+    // 1. initialize a hash map
+    unordered_map<int, int> hashmap;
+    // 2. insert a new (key, value) pair
+    hashmap.insert(make_pair(0, 0));
+    hashmap.insert(make_pair(2, 3));
+    // 3. insert a new (key, value) pair or update the value of existed key
+    hashmap[1] = 1;
+    hashmap[1] = 2;
+    // 4. get the value of a specific key
+    cout << "The value of key 1 is: " << hashmap[1] << endl;
+    // 5. delete a key
+    hashmap.erase(2);
+    // 6. check if a key is in the hash map
+    if (hashmap.count(2) <= 0) {
+        cout << "Key 2 is not in the hash map." << endl;
+    }
+    // 7. get the size of the hash map
+    cout << "the size of hash map is: " << hashmap.size() << endl; 
+    // 8. iterate the hash map
+    for (auto it = hashmap.begin(); it != hashmap.end(); ++it) {
+        cout << "(" << it->first << "," << it->second << ") ";
+    }
+    cout << "are in the hash map." << endl;
+    // 9. clear the hash map
+    hashmap.clear();
+    // 10. check if the hash map is empty
+    if (hashmap.empty()) {
+        cout << "hash map is empty now!" << endl;
+    }
+}

c71d7ca32903a18833fb8948bc8e76e9b0c07246
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2023-04-16 23:39:13 +0800

    Update 摩尔投票.cpp

--- 其他/摩尔投票.cpp
+++ 其他/摩尔投票.cpp
@@ -1,11 +1,12 @@
 //找到数组中出现次数大于n / 2的元素
 //如果它与计数器所记录的元素相同，则计数器加一，否则计数器减  一。当计数器减到 0 时，就重新选择一个元素作为候选元素，直到遍历完整个数组。
-int majorityElement(vector<int>& nums) {
-        int cnt = 0, can = 0;
-        for(int num : nums)
-        {
-            if(cnt == 0)can = num; //初始化
-            cnt += (num == can) ? 1 : -1; 
-        }
-        return can;
-    }
+int majorityElement(vector<int>& nums) 
+{
+     int cnt = 0, can = 0;
+     for(int num : nums)
+     {
+         if(cnt == 0)can = num; //初始化
+         cnt += (num == can) ? 1 : -1; 
+     }
+     return can;
+}

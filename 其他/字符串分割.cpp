33449cc74635e45ea5eb858cbb60350162b7cbfb
na2co3hk <99656524+na2co3hk@users.noreply.github.com>
2023-04-23 13:41:43 +0800

    Update 字符串分割.cpp

--- 其他/字符串分割.cpp
+++ 其他/字符串分割.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+//返回值:字符数组 split(s:待分割字符, c:分割标志)
+vector<string> split(const string& s, const string& c) {
+	size_t pos1, pos2;
+	vector<string>v;
+	pos2 = s.find(c);
+	pos1 = 0;
+	while (string::npos != pos2)
+	{
+		v.push_back(s.substr(pos1, pos2 - pos1));
+		pos1 = pos2 + c.size();
+		pos2 = s.find(c, pos1);
+	}
+	if (pos1 != s.length())v.push_back(s.substr(pos1));
+	return v;
+}
+
 //split(s:待分割字符, v:传出字符数组, c:分割标志)
 void split(const string& s, vector<string>& v, const string& c) {
 	size_t pos1, pos2;
